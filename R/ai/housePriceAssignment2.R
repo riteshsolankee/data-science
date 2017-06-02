dd1122600d28d86a97288f38a6934150094dac8f
ritesh kumar <riteshsolankee@gmail.com>
2017-06-02 23:26:20 +0530

    stacking

--- R/ai/housePriceAssignment2.R
+++ R/ai/housePriceAssignment2.R
@@ -7,18 +7,26 @@ library(data.table)
 library(caret)
 library(Boruta)
 library(Hmisc, quietly=TRUE) ## For finding the missing value using 'describe function'
+require(plyr) ## for 'llply' function
+library(knitr)
+library(dplyr)
+library(xgboost)
+library(ranger)
+library(nnet)
+library(Metrics)
+library(gbm)
 
 ### Set the working directory
 setwd("/Users/ritesh/pad-datascience/R/")
 
 ### Load data and intial analysis
-train <- read.csv("ai/data/train.csv", stringsAsFactors = F)
-test <- read.csv("ai/data/test.csv", stringsAsFactors = F)
+train.df <- read.csv("ai/data/train.csv", stringsAsFactors = F)
+test.df <- read.csv("ai/data/test.csv", stringsAsFactors = F)
 
-str(train)
-str(test)
-names(train)
-names(test)
+str(train.df)
+str(test.df)
+names(train.df)
+names(test.df)
 # Description:
 # The Ames Housing dataset was retrieved from https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data.
 # The dataset represents residential properties in Ames, Iowa from 2006 to 2010. There is a train and a test file.
@@ -37,17 +45,16 @@ names(test)
 ## so we’ll need to convert these predictions back to their original form later
 
 ## Determine data types in the data set
-ATTRS <- names(train)
-data_types <- sapply(ATTRS,function(x){class(train[[x]])})
+ATTRS <- names(train.df)
+data_types <- sapply(ATTRS,function(x){class(train.df[[x]])})
 unique_data_types <- unique(data_types)
 
 ## Separate attributes by data type
 DATA_ATTR_TYPES <- lapply(unique_data_types,function(x){ names(data_types[data_types == x])})
 names(DATA_ATTR_TYPES) <- unique_data_types
 
-
-Num_NA<-sapply(train,function(y)length(which(is.na(y)==T)))
-NA_Count<- data.frame(Item=colnames(train),Count=Num_NA)
+Num_NA<-sapply(train.df,function(y)length(which(is.na(y)==T)))
+NA_Count<- data.frame(Item=colnames(train.df),Count=Num_NA)
 NA_Count
 
 ## function to calculate 'mode'
@@ -57,19 +64,24 @@ Mode <- function(x) {
 }
 
 ## fix charater missing values
-imputeMissingAttributes <- function(df, DATA_ATTR_TYPES){
+imputeMissingAttributes <- function(df){
+  
   # for character  attributes set missing value
   char_attr <- intersect(names(df),DATA_ATTR_TYPES$character)
   for (x in char_attr){
     print(x)
-    missing_count_percentage <- describe(df[, x])$counts[2]/describe(df[, x])$counts[1]
+    print(describe(df[, x])$counts[2])
+    print(describe(df[, x])$counts[1])
+    misingVal <- describe(df[, x])$counts[2]
+    nVal <- describe(df[, x])$counts[1]
+    missing_count_percentage <- as.integer(misingVal)/as.integer(nVal)
     print(missing_count_percentage)
-    if(missing_count_percentage < 0.01){
+    if(missing_count_percentage < 0.015){
       df[[x]][is.na(df[[x]])] <- Mode(df[, x])
       #df[[x]] <- factor(df[[x]])
     }
   }
-  # for numeric set missing values to median is the mising value percentage is less than 0.01
+  # for numeric set missing values to median is the mising value percentage is less than 0.015
   num_attr <- intersect(names(df),DATA_ATTR_TYPES$integer)
   for (x in num_attr){
     print(x)
@@ -79,7 +91,7 @@ imputeMissingAttributes <- function(df, DATA_ATTR_TYPES){
     nVal <- describe(df[, x])$counts[1]
     missing_count_percentage <- as.integer(misingVal)/as.integer(nVal)
     print(missing_count_percentage)
-    if(missing_count_percentage < 0.01){
+    if(missing_count_percentage < 0.015){
       df[[x]][is.na(df[[x]])] <- median(df[, x], na.rm=TRUE)
     }
   }
@@ -101,7 +113,7 @@ imputeMissingAttributes <- function(df, DATA_ATTR_TYPES){
   df$GarageType[which(is.na(df$GarageType))]="NoGarage"
   ### As per the Project Discription NA values are replaced to No Grage
   df$GarageFinish[which(is.na(df$GarageFinish))]="NoGarage"
-  
+  df$GarageYrBlt[which(df$GarageYrBlt==2207)]=2007
   ### As per the Project discription NA values are replaced to "NoBasement"
   df$BsmtCond[which(is.na(df$BsmtCond))]="NoBasement"
   df$BsmtExposure[which(is.na(df$BsmtExposure))]="NoBasement"
@@ -123,23 +135,29 @@ imputeMissingAttributes <- function(df, DATA_ATTR_TYPES){
   df
 }
 
-train_df <- imputeMissingAttributes(train, DATA_ATTR_TYPES)
+train_df <- imputeMissingAttributes(train.df)
 
-Num_NA <- sapply(train_df,function(y)length(which(is.na(y)==T)))
-NA_Count <- data.frame(Item=colnames(train_df),Count=Num_NA)
-NA_Count
+Num_NA_train <- sapply(train_df,function(y)length(which(is.na(y)==T)))
+NA_Count_train <- data.frame(Item=colnames(train_df),Count=Num_NA_train)
+NA_Count_train
+
+test_df <- imputeMissingAttributes(test.df)
+
+Num_NA_test <- sapply(test_df,function(y)length(which(is.na(y)==T)))
+NA_Count_test <- data.frame(Item=colnames(test_df),Count=Num_NA_test)
+NA_Count_test
 #############################################################################################
 ########## Features selection (using Boruta) #############
 #############################################################################################
 
-extractFeatures <- function(train.df){
+extractFeatures <- function(train.dataframe){
   # extract only candidate feature names i.e exclude 'ID' and lable column
-  candidate.features <- setdiff(names(train.df),c("Id","SalePrice"))
-  data.type <- sapply(candidate.features,function(x){class(train.df[[x]])})
+  candidate.features <- setdiff(names(train.dataframe),c("Id","SalePrice"))
+  data.type <- sapply(candidate.features,function(x){class(train.dataframe[[x]])})
   table(data.type)
   # Determine data types
-  explanatory.attributes <- setdiff(names(train.df),c("Id","SalePrice"))
-  data.classes <- sapply(explanatory.attributes,function(x){class(train.df[[x]])})
+  explanatory.attributes <- setdiff(names(train.dataframe),c("Id","SalePrice"))
+  data.classes <- sapply(explanatory.attributes,function(x){class(train.dataframe[[x]])})
   
   # categorize data types in the data set?
   unique.classes <- unique(data.classes)
@@ -149,23 +167,14 @@ extractFeatures <- function(train.df){
   
   # Prepare data set for Boruta analysis. As Boruta uses Random forest, missng data needs to be fixed
   # pull out the response variable
-  response <- train.df$SalePrice
+  response <- train.dataframe$SalePrice
   
   # remove identifier and response variables
-  train.df <- train.df[candidate.features]
-  
-  # for numeric set missing values to -1 for purposes of the random forest run
-  for (x in attr.data.types$integer){
-    train.df[[x]][is.na(sample.df[[x]])] <- -1
-  }
-  # for charater set missing values to *MISSING* for purposes of the random forest run
-  for (x in attr.data.types$character){
-    train.df[[x]][is.na(train.df[[x]])] <- "*MISSING*"
-  }
+  train.dataframe <- train.dataframe[candidate.features]
   
   ### Run Boruta Analysis ###
   set.seed(13)
-  bor.results <- Boruta(train.df,response,
+  bor.results <- Boruta(train.dataframe,response,
                         maxRuns=101,
                         doTrace=0)
   
@@ -175,7 +184,7 @@ extractFeatures <- function(train.df){
   CONFIRMED_ATTR <- getSelectedAttributes(bor.results, withTentative = F)
   CONFIRMED_ATTR_WITH_TENTATIVE <- getSelectedAttributes(bor.results, withTentative = T)
   TENTATIVE_ATTR <- CONFIRMED_ATTR_WITH_TENTATIVE[which(!CONFIRMED_ATTR_WITH_TENTATIVE %in% CONFIRMED_ATTR)]
-  REJECTED_ATTR <- names(train.df)[!(names(train.df) %in% CONFIRMED_ATTR_WITH_TENTATIVE)]
+  REJECTED_ATTR <- names(train.dataframe)[!(names(train.dataframe) %in% CONFIRMED_ATTR_WITH_TENTATIVE)]
   PREDICTOR_ATTR <- c(CONFIRMED_ATTR,TENTATIVE_ATTR,REJECTED_ATTR)
   
   return(list(
@@ -189,18 +198,18 @@ extractFeatures <- function(train.df){
 attr_list <- extractFeatures(train_df)
 # create folds for training
 set.seed(13)
-data_folds <- createFolds(train$SalePrice, k=5)
+data_folds <- createFolds(train_df$SalePrice, k=5)
 
 ### plot the boruta variable importance chart.
 # Blue boxplots correspond to minimal, average and maximum Z score of a shadow attribute. Red, yellow and green boxplots
 # represent Z scores of rejected, tentative and confirmed attributes respectively.
-plot(bor.results, xlab = "", xaxt = "n")
-lz<-lapply(1:ncol(bor.results$ImpHistory),function(i)bor.results$ImpHistory[is.finite(bor.results$ImpHistory[,i]),i])
-names(lz) <- colnames(bor.results$ImpHistory)
+plot(attr_list$BOR_RESULT, xlab = "", xaxt = "n")
+lz<-lapply(1:ncol(attr_list$BOR_RESULT$ImpHistory),function(i)attr_list$BOR_RESULT$ImpHistory[is.finite(attr_list$BOR_RESULT$ImpHistory[,i]),i])
+names(lz) <- colnames(attr_list$BOR_RESULT$ImpHistory)
 Labels <- sort(sapply(lz,median))
-axis(side = 1,las=2,labels = names(Labels), at = 1:ncol(bor.results$ImpHistory), cex.axis = 0.7)
+axis(side = 1,las=2,labels = names(Labels), at = 1:ncol(attr_list$BOR_RESULT$ImpHistory), cex.axis = 0.7)
 
-attStats(bor.results)
+attStats(attr_list$BOR_RESULT)
 
 #########   ------  ###############
 #final.boruta <- TentativeRoughFix(bor.results)
@@ -216,3 +225,245 @@ ggplot(train, aes(x=SalePrice)) + geom_histogram(col = 'white') + theme_light() 
 summary(train[,c("SalePrice")])
 #Normalize distribution
 ggplot(train, aes(x=log(SalePrice+1))) + geom_histogram(col = 'white') + theme_light()
+
+
+###################################################################################################
+###########   Model Stacking      #################
+###################################################################################################
+
+## Feature Set 1 - Boruta Confirmed and tentative Attributes
+prepL0FeatureSet1 <- function(df) {
+  id <- df$Id
+  if (class(df$SalePrice) != "NULL") {
+    y <- log(df$SalePrice)
+  } else {
+    y <- NULL
+  }
+  predictor_vars <- c(attr_list$CONFIRMED_ATTR,attr_list$TENTATIVE_ATTR)
+  predictors <- df[predictor_vars]
+  # for character  atributes set missing value
+  char_attr <- intersect(predictor_vars,DATA_ATTR_TYPES$character)
+  for (x in char_attr){
+    predictors[[x]] <- factor(predictors[[x]])
+  }
+  return(list(id=id,y=y,predictors=predictors))
+}
+
+L0FeatureSet1 <- list(
+  train=prepL0FeatureSet1(train_df),
+  test=prepL0FeatureSet1(test_df))
+
+## Feature Set 2 (xgboost) - Boruta Confirmed Attributes
+prepL0FeatureSet2 <- function(df) {
+  id <- df$Id
+  if (class(df$SalePrice) != "NULL") {
+    y <- log(df$SalePrice)
+  } else {
+    y <- NULL
+  }
+  predictor_vars <- c(attr_list$CONFIRMED_ATTR,attr_list$TENTATIVE_ATTR)
+  predictors <- df[predictor_vars]
+  
+  # for character  atributes set missing value
+  char_attr <- intersect(predictor_vars,DATA_ATTR_TYPES$character)
+  for (x in char_attr){
+    predictors[[x]] <- as.numeric(factor(predictors[[x]]))
+  }
+  
+  return(list(id=id,y=y,predictors=as.matrix(predictors)))
+}
+L0FeatureSet2 <- list(
+  train=prepL0FeatureSet2(train_df),                    
+  test=prepL0FeatureSet2(test_df))
+
+## Helper Function For Training. train model on one data fold
+trainOneFold <- function(this_fold,feature_set) {
+  # get fold specific Cross Validation data
+  cv.data <- list()
+  cv.data$predictors <- feature_set$train$predictors[this_fold,]
+  cv.data$ID <- feature_set$train$id[this_fold]
+  cv.data$y <- feature_set$train$y[this_fold]
+  
+  # get training data for specific fold
+  train.data <- list()
+  train.data$predictors <- feature_set$train$predictors[-this_fold,]
+  train.data$y <- feature_set$train$y[-this_fold]
+  
+  set.seed(825)
+  fitted_mdl <- do.call(train,
+                        c(
+                          list(x=train.data$predictors,y=train.data$y),
+                          CARET.TRAIN.PARMS,
+                          MODEL.SPECIFIC.PARMS,
+                          CARET.TRAIN.OTHER.PARMS))
+  
+  yhat <- predict(fitted_mdl,newdata = cv.data$predictors,type = "raw")
+  score <- rmse(cv.data$y,yhat)
+  ans <- list(fitted_mdl=fitted_mdl,
+              score=score,
+              predictions=data.frame(ID=cv.data$ID,yhat=yhat,y=cv.data$y))
+  return(ans)
+}
+
+######### GBM Model #####################
+# set caret training parameters
+CARET.TRAIN.PARMS <- list(method="gbm")   
+
+CARET.TUNE.GRID <-  expand.grid(n.trees=100, 
+                                interaction.depth=10, 
+                                shrinkage=0.1,
+                                n.minobsinnode=10)
+MODEL.SPECIFIC.PARMS <- list(verbose=0) #NULL # Other model specific parameters
+# model specific training parameter
+CARET.TRAIN.CTRL <- trainControl(method="none",
+                                 verboseIter=FALSE,
+                                 classProbs=FALSE)
+CARET.TRAIN.OTHER.PARMS <- list(trControl=CARET.TRAIN.CTRL,
+                                tuneGrid=CARET.TUNE.GRID,
+                                metric="RMSE")
+
+# generate features for Level 1
+gbm_set <- llply(data_folds,trainOneFold,L0FeatureSet1)
+gbm_set
+# final model fit
+gbm_mdl <- do.call(train,
+                   c(list(x=L0FeatureSet1$train$predictors,y=L0FeatureSet1$train$y),
+                     CARET.TRAIN.PARMS,
+                     MODEL.SPECIFIC.PARMS,
+                     CARET.TRAIN.OTHER.PARMS))
+
+# Cross validation Error Estimate
+cv_y <- do.call(c,lapply(gbm_set,function(x){x$predictions$y}))
+cv_yhat <- do.call(c,lapply(gbm_set,function(x){x$predictions$yhat}))
+rmse(cv_y,cv_yhat)
+
+cat("Average CV rmse:",mean(do.call(c,lapply(gbm_set,function(x){x$score}))))
+
+# create test submission.
+# A prediction is made by averaging the predictions made by using the models
+# fitted for each fold.
+test_gbm_yhat <- predict(gbm_mdl,newdata = L0FeatureSet1$test$predictors,type = "raw")
+gbm_submission <- cbind(Id=L0FeatureSet1$test$id,SalePrice=exp(test_gbm_yhat))
+write.csv(gbm_submission,file="/Users/ritesh/Documents/DataScience/AdvancedML/gbm_sumbission.csv",row.names=FALSE)
+
+####################  xgboost ################
+# set caret training parameters
+CARET.TRAIN.PARMS <- list(method="xgbTree")   
+CARET.TUNE.GRID <-  expand.grid(nrounds=800, 
+                                max_depth=10, 
+                                eta=0.03, 
+                                gamma=0.1, 
+                                colsample_bytree=0.4, 
+                                min_child_weight=1,
+                                subsample = 1)
+MODEL.SPECIFIC.PARMS <- list(verbose=0) #NULL # Other model specific parameters
+# model specific training parameter
+CARET.TRAIN.CTRL <- trainControl(method="none",
+                                 verboseIter=FALSE,
+                                 classProbs=FALSE)
+CARET.TRAIN.OTHER.PARMS <- list(trControl=CARET.TRAIN.CTRL,
+                                tuneGrid=CARET.TUNE.GRID,
+                                metric="RMSE")
+# generate Level 1 features
+xgb_set <- llply(data_folds,trainOneFold,L0FeatureSet2)
+# final model fit
+xgb_mdl <- do.call(train,
+                   c(list(x=L0FeatureSet2$train$predictors,y=L0FeatureSet2$train$y),
+                     CARET.TRAIN.PARMS,
+                     MODEL.SPECIFIC.PARMS,
+                     CARET.TRAIN.OTHER.PARMS))
+# CV Error Estimate
+cv_y <- do.call(c,lapply(xgb_set,function(x){x$predictions$y}))
+cv_yhat <- do.call(c,lapply(xgb_set,function(x){x$predictions$yhat}))
+rmse(cv_y,cv_yhat)
+cat("Average CV rmse:",mean(do.call(c,lapply(xgb_set,function(x){x$score}))))
+# create test submission.
+# A prediction is made by averaging the predictions made by using the models
+# fitted for each fold.
+test_xgb_yhat <- predict(xgb_mdl,newdata = L0FeatureSet2$test$predictors,type = "raw")
+xgb_submission <- cbind(Id=L0FeatureSet2$test$id,SalePrice=exp(test_xgb_yhat))
+
+write.csv(xgb_submission,file="/Users/ritesh/Documents/DataScience/AdvancedML/xgb_sumbission.csv",row.names=FALSE)
+
+########################### ranger Model   ############################
+# set caret training parameters
+CARET.TRAIN.PARMS <- list(method="ranger")   
+CARET.TUNE.GRID <-  expand.grid(mtry=2*as.integer(sqrt(ncol(L0FeatureSet1$train$predictors))))
+MODEL.SPECIFIC.PARMS <- list(verbose=0,num.trees=500) #NULL # Other model specific parameters
+# model specific training parameter
+CARET.TRAIN.CTRL <- trainControl(method="none",
+                                 verboseIter=FALSE,
+                                 classProbs=FALSE)
+CARET.TRAIN.OTHER.PARMS <- list(trControl=CARET.TRAIN.CTRL,
+                                tuneGrid=CARET.TUNE.GRID,
+                                metric="RMSE")
+# generate Level 1 features
+rngr_set <- llply(data_folds,trainOneFold,L0FeatureSet1)
+# final model fit
+rngr_mdl <- do.call(train,
+                    c(list(x=L0FeatureSet1$train$predictors,y=L0FeatureSet1$train$y),
+                      CARET.TRAIN.PARMS,
+                      MODEL.SPECIFIC.PARMS,
+                      CARET.TRAIN.OTHER.PARMS))
+# CV Error Estimate
+cv_y <- do.call(c,lapply(rngr_set,function(x){x$predictions$y}))
+cv_yhat <- do.call(c,lapply(rngr_set,function(x){x$predictions$yhat}))
+rmse(cv_y,cv_yhat)
+cat("Average CV rmse:",mean(do.call(c,lapply(rngr_set,function(x){x$score}))))
+
+# create test submission.
+# A prediction is made by averaging the predictions made by using the models
+# fitted for each fold.
+test_rngr_yhat <- predict(rngr_mdl,newdata = L0FeatureSet1$test$predictors,type = "raw")
+rngr_submission <- cbind(Id=L0FeatureSet1$test$id,SalePrice=exp(test_rngr_yhat))
+
+write.csv(rngr_submission,file="/Users/ritesh/Documents/DataScience/AdvancedML/rngr_sumbission.csv",row.names=FALSE)
+
+########################### Level 1 Model Training ########################
+gbm_yhat <- do.call(c,lapply(gbm_set,function(x){x$predictions$yhat}))
+xgb_yhat <- do.call(c,lapply(xgb_set,function(x){x$predictions$yhat}))
+rngr_yhat <- do.call(c,lapply(rngr_set,function(x){x$predictions$yhat}))
+
+# create Feature Set
+L1FeatureSet <- list()
+
+L1FeatureSet$train$id <- do.call(c,lapply(gbm_set,function(x){x$predictions$ID}))
+L1FeatureSet$train$y <- do.call(c,lapply(gbm_set,function(x){x$predictions$y}))
+predictors <- data.frame(gbm_yhat,xgb_yhat,rngr_yhat)
+predictors_rank <- t(apply(predictors,1,rank))
+colnames(predictors_rank) <- paste0("rank_",names(predictors))
+L1FeatureSet$train$predictors <- predictors #cbind(predictors,predictors_rank)
+
+L1FeatureSet$test$id <- gbm_submission[,"Id"]
+L1FeatureSet$test$predictors <- data.frame(gbm_yhat=test_gbm_yhat,
+                                           xgb_yhat=test_xgb_yhat,
+                                           rngr_yhat=test_rngr_yhat)
+############################ Neural Net Model ################################
+# set caret training parameters
+CARET.TRAIN.PARMS <- list(method="nnet") 
+CARET.TUNE.GRID <-  NULL  # NULL provides model specific default tuning parameters
+# model specific training parameter
+CARET.TRAIN.CTRL <- trainControl(method="repeatedcv",
+                                 number=5,
+                                 repeats=1,
+                                 verboseIter=FALSE)
+CARET.TRAIN.OTHER.PARMS <- list(trControl=CARET.TRAIN.CTRL,
+                                maximize=FALSE,
+                                tuneGrid=CARET.TUNE.GRID,
+                                tuneLength=7,
+                                metric="RMSE")
+MODEL.SPECIFIC.PARMS <- list(verbose=FALSE,linout=TRUE,trace=FALSE) #NULL # Other model specific parameters
+# train the model
+set.seed(825)
+l1_nnet_mdl <- do.call(train,c(list(x=L1FeatureSet$train$predictors,y=L1FeatureSet$train$y),
+                               CARET.TRAIN.PARMS,
+                               MODEL.SPECIFIC.PARMS,
+                               CARET.TRAIN.OTHER.PARMS))
+l1_nnet_mdl
+cat("Average CV rmse:",mean(l1_nnet_mdl$resample$RMSE),"\n")
+
+test_l1_nnet_yhat <- predict(l1_nnet_mdl,newdata = L1FeatureSet$test$predictors,type = "raw")
+l1_nnet_submission <- cbind(Id=L1FeatureSet$test$id,SalePrice=exp(test_l1_nnet_yhat))
+colnames(l1_nnet_submission) <- c("Id","SalePrice")
+
+write.csv(l1_nnet_submission,file="/Users/ritesh/Documents/DataScience/AdvancedML/l1_nnet_submission.csv",row.names=FALSE)
